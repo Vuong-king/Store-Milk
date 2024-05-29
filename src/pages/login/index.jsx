261da216a0bbc485dc8c56a5ce2039e22c2ffbe6
trung-trun <trungncse170145@fpt.edu.vn>
2024-05-29 09:40:28 +0000

    trunng

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,34 +1,32 @@
-import "./index.scss";
+import "./login.scss";
 
 function Login() {
     return (
-        <div className="login">
-            <div className="wrapper">
-                <form className="login__form" action="">
+        <div className="login-container">
+            <div className="login-wrapper">
+                <form className="login-form" action="">
                     <h1>Đăng nhập</h1>
-                    <div className="input-box">
+                    <div className="input-group">
                         <input type="text" placeholder="Username" />
                     </div>
-                    <div className="input-box">
+                    <div className="input-group">
                         <input type="password" placeholder="Your password" />
-
                     </div>
 
-                    <div className="remeber-forgot">
+                    <div className="remember-forgot">
                         <label>
                             <input type="checkbox" />Ghi nhớ mật khẩu
                         </label>
                         <a href="#">Quên mật khẩu?</a>
                     </div>
-                    <button className="done" type="submit">Đăng nhập</button>
-                    <div className="register">
-                        <p>Bạn chưa có tài khoản    <a href="#">Đăng ký</a> </p>
+                    <button className="login-button" type="submit">Đăng nhập</button>
+                    <div className="register-link">
+                        <p>Bạn chưa có tài khoản <a href="#">Đăng ký</a></p>
                     </div>
-                    
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
